cd37cfafb07e7db8984a8fc5ba59104cdc61f455
Jose Lora <jose_lora@mckinsey.com>
2022-11-07 15:17:47 -0500

    Added ES translation

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -3,9 +3,10 @@ import i18n from "i18n-js"
 import { I18nManager } from "react-native"
 
 // if English isn't your default language, move Translations to the appropriate language file.
-import en, { Translations } from "./en"
+import es, { Translations } from "./es"
 import ar from "./ar"
 import ko from "./ko"
+import en from "./en"
 
 i18n.fallbacks = true
 /**
@@ -13,7 +14,7 @@ i18n.fallbacks = true
  * the language code is the suffixed with "-US". i.e. if a device is set to English ("en"),
  * if you change to another language and then return to English language code is now "en-US".
  */
-i18n.translations = { ar, en, "en-US": en, ko }
+i18n.translations = { ar, en, "en-US": en, ko, es }
 
 i18n.locale = Localization.locale
 
